9296c619c8f77bb4eeabcbb6facafeeb71ebae73
serafim <73206276+Vinicin1101@users.noreply.github.com>
2024-09-28 23:40:49 +0000

    3D version

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Card } from '../components/Card/Card';
 import Game from '../components/Game/Game';
 import './App.css';
+import SceneCanva from '../components/Scene/Scene';
 
 function App() {
   const [attempt, setAttempt] = useState<number>(0);
@@ -22,7 +23,7 @@ function App() {
         <h1>LUCKY DICE</h1>
       </header>
       <section id='container-game'>
-        <Game setAttempt={setAttempt} onWin={handleWin} attempt={attempt} />
+        <SceneCanva />
       </section>
       {showCard && (
         <Card attempt={attempt} onRestart={handleRestart} />
